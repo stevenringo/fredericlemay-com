9d602aedc52d97c100891409426f43975fc4209e
flemay <flemay@noreplyplease.com>
2018-08-16 23:14:32 +1000

    use postcss pluging purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,23 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-    plugins: {
-        tailwindcss: './tailwind.js',
-        autoprefixer: {
+    plugins: [
+        tailwindcss('./tailwind.js'),
+        purgecss({
+            content: ['layouts/**/*.html'],
+            css: ['assets/css/main.css'],
+            extractors: [{
+                extractor: class {
+                    static extract(content) {
+                        return content.match(/[A-z0-9-:\/]+/g) || [];
+                    }
+                },
+                extensions: ['html']
+            }]
+        }),
+        autoprefixer({
             browsers: [
                 "Android 2.3",
                 "Android >= 4",
@@ -12,6 +28,6 @@ module.exports = {
                 "Opera >= 12",
                 "Safari >= 6"
             ]
-        }
-    },
+        })
+    ]
 }
